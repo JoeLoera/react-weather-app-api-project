fbbdce7d6c2415f935f82fcaa85b5ea4bbb1203b
Joe Loera <joeloera7@gmail.com>
2023-07-02 11:26:50 -0700

    Practice with restcountries api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+
+  const [ countries, setCountries] = useState(null)
+
+
+  const getData = async() => {
+    const response = await fetch("https://restcountries.com/v3.1/all")
+    const json = await response.json()
+    setCountries(json)
+  }
+
+  useEffect(() => getData, [])
+
+  console.log(countries)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {countries?.map(country => <h1>{country.name.common}</h1>)}
     </div>
   );
 }
