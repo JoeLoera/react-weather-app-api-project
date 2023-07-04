fcd53cfc9c9756d88eb427e410831dc7c725207e
Joe Loera <joeloera7@gmail.com>
2023-07-03 21:44:25 -0700

    Blank slate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import { useEffect, useState } from "react";
-
 const App = () => {
-
-  const [ countries, setCountries] = useState(null)
-
-
-  const getData = async() => {
-    const response = await fetch("https://restcountries.com/v3.1/all")
-    const json = await response.json()
-    setCountries(json)
-  }
-
-  useEffect(() => getData, [])
-
-  console.log(countries)
-  return (
+return (
     <div>
-      {countries?.map(country => <h1>{country.name.common}</h1>)}
+
     </div>
   );
 }
-
 export default App;
